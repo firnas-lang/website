1beb208bb538668b41b67cba8d6853550a5d0beb
Hamza Jadid <haljadid@proximie.com>
2023-12-18 03:14:07 +0200

    feat: added description for features

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -93,26 +93,18 @@ const config: Config = {
     footer: {
       style: 'dark',
       links: [
-        {
-          title: 'Docs',
-          items: [
-            // {
-            //   label: 'Tutorial',
-            //   to: '/docs/intro',
-            // },
-          ],
-        },
+        // {
+        //   title: 'Docs',
+        //   items: [
+        //     {
+        //       label: 'Tutorial',
+        //       to: '/docs/intro',
+        //     },
+        //   ],
+        // },
         {
           title: 'Community',
           items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
             {
               label: 'Twitter',
               href: 'https://twitter.com/docusaurus',
